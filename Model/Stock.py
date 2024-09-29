2429eb45aba0b5a744ca860d407354bc1572c371
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-09-29 20:21:27 +0700

    Update flask into stocks

--- Model/Stock.py
+++ Model/Stock.py
@@ -1,11 +1,11 @@
-class Person:
-    def __init__(self, name, age):
+class Stock:
+    def __init__(self, name, price):
         self.name = name
-        self.age = age
+        self.price = price
 
     # Convert the Person object to a dictionary
     def to_dict(self):
         return {
             "name": self.name,
-            "age": self.age
+            "price": self.price
         }
